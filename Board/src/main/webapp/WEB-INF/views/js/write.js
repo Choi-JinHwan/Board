d670fd480856f7063faeadd907c96509adffbd1f
Anto <eqaz1366@naver.com>
2019-02-20 14:12:53 +0900

    commit

--- Board/src/main/webapp/WEB-INF/views/js/write.js
+++ Board/src/main/webapp/WEB-INF/views/js/write.js
@@ -1,5 +1,6 @@
 let title         = document.querySelector("#title");
 let description   = document.querySelector("#description");
+let name          = document.querySelector("#name");
 let submit        = document.querySelector("#submit");
 let submitForm    = document.querySelector("#submitForm");
 
@@ -10,11 +11,19 @@ let checkForm = function(){
       alert("제목을 작성해주세요.");
       title.focus();
       e.preventDefault();
+      return false;
     }
+    if (name.value == "") {
+      alert("이름을 작성해주세요.");
+      name.focus();
+      e.preventDefault();
+      return false;
+    } 
     if (description.value == "") {
       alert("내용을 작성해주세요.");
       description.focus();
       e.preventDefault();
+      return false;
     } 
   });
 }
