afbef39ae0948a4eb1d6375d1a0663955e74e8b9
Anto <eqaz1366@naver.com>
2019-02-21 18:28:51 +0900

    mapper comment add, update add

--- Board/src/main/java/com/board/www/controller/PostController.java
+++ Board/src/main/java/com/board/www/controller/PostController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.www.dto.PostVO;
 import com.board.www.service.PostService;
 
 @Controller
@@ -13,15 +15,46 @@ public class PostController {
 	@Autowired
 	PostService postService;
 	
+	//게시물 목록 조회
 	@RequestMapping("/PostList")
 	public String postListController(Model model) {
+		//게시물 목록을 받아 모델에 넣는다.
 		model.addAttribute("postList",postService.getPostList());
 		return "PostList";
 	}
+	
+	//게시물 조회
 	@RequestMapping("/PostView/{postId}")
 	public String postViewController(@PathVariable int postId, Model model) {
+		//게시물을 받아 모델에 넣는다.
 		model.addAttribute("post",postService.getPost(postId));
+		//해당 게시물의 조회수를 상승시킨다.
 		postService.updateViewCount(postId);
 		return "PostView";
+	}
+	
+	//게시물 수정 폼으로 이동
+	@RequestMapping("/PostUpdate/{postId}")
+	public String postUpdateWriter(@PathVariable int postId, Model model) {
+		model.addAttribute("post", postService.getPost(postId));
+		return "PostUpdate";
+	}
+	
+	@RequestMapping("/PostUpdate")
+	public String postUpdate(
+			Model model, 
+			@RequestParam String title, 
+			@RequestParam String contents, 
+			@RequestParam String postId
+			) {
+		PostVO post = new PostVO();
+		post.setPostId(Integer.valueOf(postId));
+		post.setContents(contents);
+		post.setTitle(title);
+		
+		postService.updatePost(post);
+		
+		return "PostList";
+		
 	}
 }
--- Board/src/main/java/com/board/www/service/PostServiceImpl.java
+++ Board/src/main/java/com/board/www/service/PostServiceImpl.java
@@ -14,31 +14,31 @@ public class PostServiceImpl implements PostService{
 	@Autowired
 	PostDAO postDao;
 	
-	@Override
+	@Override	//게시물 목록 조회
 	public List<PostVO> getPostList() {
 		
 		return postDao.getPostList();
 	}
 
-	@Override
+	@Override	//게시물 조회
 	public PostVO getPost(int postId) {
 		
 		return postDao.getPost(postId);
 	}
 
-	@Override
+	@Override	//게시물 수정
 	public boolean updatePost(PostVO post) {
 		
 		return postDao.updatePost(post);
 	}
 
-	@Override
+	@Override	//게시물 삭제
 	public boolean deletePost(int postId) {
 		
 		return postDao.deletePost(postId);
 	}
 
-	@Override
+	@Override	//게시물 조회수 상승
 	public void updateViewCount(int postId) {
 		
 		postDao.updateViewCount(postId);
